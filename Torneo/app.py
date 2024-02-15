ed02b11360c9b98a208e5c865ae6e8539a129082
carlosgabar <carlosgabaponte@gmail.com>
2024-02-15 16:23:15 -0400

    subir proyecto

---
+++ Torneo/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template,request,redirect,session
+from datetime import datetime
+from flask import send_from_directory
+import os
+
+app=Flask(__name__,template_folder='templates')
+app.secret_key="torneo"
+
+@app.route('/')
+def inicio():
+
+    return render_template('menu.html')
+
+@app.route('/login_admin')
+def login():
+
+    return render_template('login_admin.html')
+
+if __name__=='__main__':
+
+    app.run(debug=True)
+
+
